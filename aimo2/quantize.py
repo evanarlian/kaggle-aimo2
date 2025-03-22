5c5db7897b9d35288f97b7abfd4e7b919a8e0290
Evan Arlian <evanarlian2000@gmail.com>
2025-03-22 12:17:56 +0700

    awq

---
+++ aimo2/quantize.py
@@ -0,0 +1,38 @@
+# https://github.com/casper-hansen/AutoAWQ/blob/main/examples/quantize.py
+from argparse import ArgumentParser
+
+from awq import AutoAWQForCausalLM
+from huggingface_hub import upload_folder
+from transformers import AutoTokenizer
+
+
+def main(model_path: str, revision: str):
+    # 1. quantize
+    quant_name = model_path.rsplit("/", maxsplit=1)[-1] + "-AWQ"
+    quant_config = {
+        "zero_point": True,
+        "q_group_size": 128,
+        "w_bit": 4,
+        "version": "GEMM",
+    }
+    model = AutoAWQForCausalLM.from_pretrained(model_path, revision=revision)
+    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+    model.quantize(tokenizer, quant_config=quant_config)
+    # 2. save to local filesystem
+    model.save_quantized(quant_name)
+    tokenizer.save_pretrained(quant_name)
+    print(f"Quantized: {quant_name}")
+    # 3. push to hub
+    repo_id = f"evanarlian/{quant_name}"
+    upload_folder(folder_path=quant_name, repo_id=repo_id)
+    model.push_to_hub(quant_name)
+    print(f"Pushed: {repo_id}")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--model-path", type=str, required=True, help="hf model")
+    parser.add_argument("--revision", type=str, required=True, help="commit hash")
+    args = parser.parse_args()
+    print(args)
+    main(args.model_path, args.revision)
