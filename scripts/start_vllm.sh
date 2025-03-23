52d1dbcf9135209c8826c6ed4a43cfcf07bfb991
Evan Arlian <evanarlian2000@gmail.com>
2025-03-23 17:06:04 +0700

    add code to change model

--- scripts/start_vllm.sh
+++ scripts/start_vllm.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 CLONES=1
+MODEL=evanarlian/DeepScaleR-1.5B-Preview-AWQ
+# MODEL=casperhansen/deepseek-r1-distill-qwen-1.5b-awq
 
 for i in $(seq 0 $((CLONES - 1))); do
-    CUDA_VISIBLE_DEVICES=$i uv run vllm serve \
-        casperhansen/deepseek-r1-distill-qwen-1.5b-awq \
-        --port=$((8001 + i)) &
+    CUDA_VISIBLE_DEVICES=$i uv run vllm serve $MODEL --port=$((8001 + i)) &
 done
 
 nginx -p nginx -c conf/nginx.conf
